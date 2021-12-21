87efbce6df0e3b4268e2062fea896ecbbae6acb4
ruandocini <ruandocini@gmail.com>
2021-12-20 23:18:38 -0300

    fix on read

--- EP1/Main.java
+++ EP1/Main.java
@@ -16,13 +16,15 @@ public class Main {
 		Matriz agregada = new Matriz(n, 1);
 
 		for (int x=0; x < n; x++){
+			int acumulator = 0;
 			for (int y=0; y < n; y++){
-				
+				System.out.println(in.hasNext());
 				String val = in.next();
 				int position = Integer.parseInt(val);
 				matriz.set(x,y,position);
+				acumulator++;
 			}
-			if (in.hasNext()){
+			if (in.hasNext() && acumulator > max_size_line){
 				agregada.set(x,0,in.nextInt());
 			}
 			// System.out.print(x+" "+y+" "+position+"\n");
