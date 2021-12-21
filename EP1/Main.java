74f82f0c42904d9f550b4fc79b9fa557c4c51a8a
ruandocini <ruandocini@gmail.com>
2021-12-20 23:08:23 -0300

    fix on readline

--- EP1/Main.java
+++ EP1/Main.java
@@ -11,33 +11,40 @@ public class Main {
 		Scanner in = new Scanner(System.in);	// Scanner para facilitar a leitura de dados a partir da entrada padrao.
 		String operacao = in.next();		// le, usando o scanner, a string que determina qual operacao deve ser realizada.
 		int n = in.nextInt();			// le a dimensão da matriz a ser manipulada pela operacao escolhida.
-		Matriz test_matriz = new Matriz(n, n);
+		Matriz matriz = new Matriz(n, n);
+		int max_size_line = n;
+		Matriz agregada = new Matriz(n, 1);
 
-		while(in.hasNext()){
-
-			for (int x=0; x < n; x++){
-				for (int y=0; y < n; y++){
-					int position = in.nextInt();
-					test_matriz.set(x,y,position);
-					// System.out.print(x+" "+y+" "+position+"\n");
-					// System.out.println(position);
-				}
+		for (int x=0; x < n; x++){
+			for (int y=0; y < n; y++){
+				
+				String val = in.next();
+				int position = Integer.parseInt(val);
+				matriz.set(x,y,position);
 			}
+			if (in.hasNext()){
+				agregada.set(x,0,in.nextInt());
+			}
+			// System.out.print(x+" "+y+" "+position+"\n");
+			// System.out.println(position);
 		}
+		
+		matriz.imprime();
+		agregada.imprime();
 
-		if("resolve".equals(operacao)){
-
-		}
-		else if("inverte".equals(operacao)){
-
-		}
-		else if("determinante".equals(operacao)){
-			double result = test_matriz.determinante(test_matriz);
-			System.out.println(result);
-		}
-		else {
-			System.out.println("Operação desconhecida!");
-			System.exit(1);
-		}
+		// if("resolve".equals(operacao)){
+		// 	matriz.calculaSolucao();
+		// }
+		// else if("inverte".equals(operacao)){
+		// 	matriz.calculaInversa();
+		// 	}
+		// else if("determinante".equals(operacao)){
+		// 	double result = matriz.determinante(matriz);
+		// 	System.out.println(result);
+		// }
+		// else {
+		// 	System.out.println("Operação desconhecida!");
+		// 	System.exit(1);
+		// }
 	}
 }
